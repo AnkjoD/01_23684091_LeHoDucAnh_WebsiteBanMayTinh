0c0898a48006a835868735f112f3993f3c0a8df4
Ankkun <ankkanjo@gmail.com>
2025-05-08 20:41:31 +0700

    #

--- js/userStatus.js
+++ js/userStatus.js
@@ -19,36 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
   
     if (!isEmpty(loggedInUser)) {
       if (loginLink) loginLink.remove();
-      account.innerHTML += `<a href = "#"><i class="fa fa-user-circle-o" style="font-size: 20px;" aria-hidden="true"></i></a>`;
+      account.innerHTML += `<a href = "#"><i class="fa fa-sign-out" aria-hidden="true" id="logoutBtn"></i></a>`;
 
-      const userDropdown = document.createElement("div");
-      userDropdown.className = "user-dropdown";
-  
-      userDropdown.innerHTML += `
-        <div class="profile" id="profile">
-       
-      </div>
-      <div class="menu">
-        <h3 class="user-name">${loggedInUser.firstName} ${loggedInUser.lastName}</h3>
-        <ul>
-           
-            <li><div id="logoutBtn">Đăng xuất</div></li>
-        </ul>
-      </div>
-      `;
-  
-      
-      userMenu.appendChild(userDropdown);
-      
-  
-      // Toggle dropdown
-      const nameToggle = userDropdown.querySelector(".user-name");
-      const details = userDropdown.querySelector(".user-details");
-  
-      nameToggle.addEventListener("click", () => {
-        details.classList.toggle("show");
-      });
-  
+    
     //   Logout
 
       document.getElementById("logoutBtn").addEventListener("click", () => {
@@ -56,10 +29,6 @@ document.addEventListener("DOMContentLoaded", function () {
         location.reload();
       });
 
-      document.getElementById("profile").addEventListener("click", () => {
-         // Chuyển sang trang cá nhân
-         const toggleMenu = document.querySelector(".menu");
-        toggleMenu.classList.toggle("active");
-      });
+      
     }
 });
